b09f7c886b146489d6dcea3e6bb44a9cf4b293f1
Will Charczuk <will@blendlabs.com>
2016-10-06 14:24:08 -0700

    initial commit.

---
+++ main_test.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	assert "github.com/blendlabs/go-assert"
+)
+
+func TestBufferInsertRowAt(t *testing.T) {
+	assert := assert.New(t)
+
+	b := buffer{}
+	edited := b.InsertRowAt(0)
+	assert.Len(edited, 1)
+}
+
+func TestBufferInsertRowAtMid(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a'},
+		[]byte{'b'},
+	}
+	edited := b.InsertRowAt(1)
+	assert.Len(edited, 3)
+	assert.Equal('a', edited[0][0])
+	assert.Len(edited[1], 0)
+	assert.Len(edited[2], 1)
+	assert.Equal('b', edited[2][0])
+}
+
+func TestBufferInsertRowAtEnd(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a'},
+		[]byte{'b'},
+	}
+	edited := b.InsertRowAt(2)
+	assert.Len(edited, 3)
+	assert.Equal('a', edited[0][0])
+	assert.Len(edited[1], 1)
+	assert.Equal('b', edited[1][0])
+	assert.Len(edited[2], 0)
+}
+
+func TestInsertAtEmptyLine(t *testing.T) {
+	assert := assert.New(t)
+
+	b := buffer{}
+	edited := b.InsertCharacterAt(0, 0, 'a')
+	assert.Len(edited, 1)
+	assert.Len(edited[0], 1)
+}
+
+func TestInsertAtEndOfLine(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a'},
+	}
+	edited := b.InsertCharacterAt(0, 1, 'b')
+	assert.Len(edited, 1)
+	assert.Len(edited[0], 2, fmt.Sprintf("%#v", edited[0]))
+	assert.Equal('b', edited[0][1])
+}
+
+func TestInsertMidLine(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a', 'b', 'c'},
+	}
+	edited := b.InsertCharacterAt(0, 1, 'd')
+	assert.Len(edited, 1)
+	assert.Len(edited[0], 4)
+	assert.Equal('a', edited[0][0])
+	assert.Equal('d', edited[0][1])
+	assert.Equal('b', edited[0][2])
+}
+
+func TestBufferRemoveCharacterAtStart(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a', 'b', 'c'},
+		[]byte{'a', 'b', 'c'},
+		[]byte{'a', 'b', 'c'},
+	}
+	edited := b.RemoveCharacterAt(1, 0)
+	assert.Len(edited, 3)
+	assert.Len(edited[0], 3)
+	assert.Len(edited[1], 2)
+	assert.Len(edited[2], 3)
+	assert.Equal('b', edited[1][0])
+	assert.Equal('c', edited[1][1])
+}
+
+func TestBufferRemoveCharacterAtMid(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a', 'b', 'c'},
+	}
+	edited := b.RemoveCharacterAt(0, 1)
+	assert.Len(edited, 1)
+	assert.Len(edited[0], 2)
+	assert.Equal('c', edited[0][1])
+}
+
+func TestBufferRemoveCharacterAtEnd(t *testing.T) {
+	assert := assert.New(t)
+
+	var b buffer = [][]byte{
+		[]byte{'a', 'b', 'c'},
+	}
+	edited := b.RemoveCharacterAt(0, 2)
+	assert.Len(edited, 1)
+	assert.Len(edited[0], 2)
+	assert.Equal('b', edited[0][1])
+}
